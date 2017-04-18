fixed training code reduced image dimension by a factor of 0.1 changed cnn hyperparameters changed codebase to python3

models/cnn.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Aman Priyadarshi
 # @Date:   2017-04-17 18:18:50
-# @Last Modified by:   Aman Priyadarshi
-# @Last Modified time: 2017-04-17 22:09:13
+# @Last Modified by:   amaneureka
+# @Last Modified time: 2017-04-19 04:02:34
 
 import numpy as np
 import tensorflow as tf
@@ -61,15 +61,15 @@ def create_network(img_height, img_width, num_classes):
     # two fully connected layers
     layer3, w3 = fc_layer(input=layer2_flat,
                           num_input=features,
-                          num_output=10)
+                          num_output=50)
     layer3 = tf.nn.tanh(layer3)
     layer4, w4 = fc_layer(input=layer3,
-                          num_input=10,
+                          num_input=50,
                           num_output=num_classes)
     y = tf.nn.softmax(layer4)
 
     # learning
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer4, labels=y_true)
     cost = tf.reduce_mean(cross_entropy)
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-5).minimize(cost)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=1e-4).minimize(cost)
     return x, y, y_true, optimizer

normalize.py
@@ -1,22 +1,35 @@
 # -*- coding: utf-8 -*-
 # @Author: amaneureka
 # @Date:   2017-04-07 17:41:23
-# @Last Modified by:   Aman Priyadarshi
-# @Last Modified time: 2017-04-17 18:05:32
+# @Last Modified by:   amaneureka
+# @Last Modified time: 2017-04-19 03:56:19
 
 import cv2
+import math
 import numpy as np
 import progressbar
 import matplotlib.pyplot as plt
 
+num_classes = 62
+num_samples = 10
 
 def find_samples_bounding_rect(path):
 
 	min_w = 0
 	min_h = 0
 
-	for i in range(1, 63):
-		for j in range(1, 56):
+	print ('finding bounding box:')
+	bar = progressbar.ProgressBar(maxval=num_classes*num_samples,
+		widgets=[
+		' [', progressbar.Timer(), '] ',
+		progressbar.Bar(),
+		' (', progressbar.ETA(), ') ',
+	])
+	bar.start()
+	counter = 0
+
+	for i in range(1, num_classes + 1):
+		for j in range(1, num_samples + 1):
 
 			filename = '{0}/Sample{1:03d}/img{1:03d}-{2:03d}.png'.format(path, i, j)
 
@@ -31,13 +44,18 @@ def find_samples_bounding_rect(path):
 			min_w = max(min_w, w)
 			min_h = max(min_h, h)
 
+			# update progress bar
+			counter = counter + 1
+			bar.update(counter)
+
+	bar.finish()
 	return min_w, min_h
 
 
-def crop_images(path, width, height, showsamples):
+def crop_images(path, width, height, showsamples, scaling):
 
-	print 'cropping images:'
-	bar = progressbar.ProgressBar(maxval=62*56,
+	print ('cropping images:')
+	bar = progressbar.ProgressBar(maxval=num_classes*num_samples,
 		widgets=[
 		' [', progressbar.Timer(), '] ',
 		progressbar.Bar(),
@@ -46,10 +64,21 @@ def crop_images(path, width, height, showsamples):
 	bar.start()
 	counter = 0
 
+	new_width = int(width * scaling + 0.5)
+	new_height = int(height * scaling + 0.5)
+
 	with open(path + '/normalized.bin', 'wb') as f:
+
+		# dump configs
+		f.write((num_classes).to_bytes(4, byteorder='little'))
+		f.write((num_samples).to_bytes(4, byteorder='little'))
+		f.write((new_width).to_bytes(4, byteorder='little'))
+		f.write((new_height).to_bytes(4, byteorder='little'))
+
+		# dump images
 		img_canvas = np.zeros((height, width), dtype=np.uint8)
-		for i in range(1, 63):
-			for j in range(1, 56):
+		for i in range(1, num_classes + 1):
+			for j in range(1, num_samples + 1):
 
 				filename = '{0}/Sample{1:03d}/img{1:03d}-{2:03d}.png'.format(path, i, j)
 
@@ -61,8 +90,8 @@ def crop_images(path, width, height, showsamples):
 				x, y, w, h = cv2.boundingRect(contours[len(contours) - 1])
 
 				# center align character
-				offset_x = (width - w) / 2
-				offset_y = (height - h) / 2
+				offset_x = int((width - w) / 2)
+				offset_y = int((height - h) / 2)
 				x = max(x - offset_x, 0)
 				y = max(y - offset_y, 0)
 				newimg = imgray[y : y + height, x : x + width]
@@ -72,8 +101,12 @@ def crop_images(path, width, height, showsamples):
 				h = newimg.shape[0]
 				img_canvas[ : h, : w] = newimg
 
+
+				# resize image
+				newimg = cv2.resize(img_canvas, None, fx=scaling, fy=scaling, interpolation = cv2.INTER_AREA)
+
 				# append ndarry to file
-				f.write(img_canvas.flatten())
+				f.write(newimg.flatten())
 
 				# update progressbar
 				counter = counter + 1
@@ -81,13 +114,15 @@ def crop_images(path, width, height, showsamples):
 
 				# preview if requested
 				if showsamples:
-					plt.imshow(img_canvas, cmap='gray')
+					plt.imshow(newimg, cmap='gray')
 					plt.show()
 
 		f.close()
 	bar.finish()
+	return new_width, new_height
 
 if __name__ == '__main__':
-	width, height = 751, 841#find_samples_bounding_rect('dataset')
-	print width, height
-	crop_images('dataset', width, height, False)
+	width, height = find_samples_bounding_rect('dataset')
+	print('Bounding Rectangle:: width: %d height: %d' % (width, height))
+	width, height = crop_images('dataset', width, height, False, 0.1)
+	print('Cropping:: width: %d height: %d' % (width, height))

train.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: Aman Priyadarshi
 # @Date:   2017-04-17 11:39:30
-# @Last Modified by:   Aman Priyadarshi
-# @Last Modified time: 2017-04-17 22:53:58
+# @Last Modified by:   amaneureka
+# @Last Modified time: 2017-04-19 03:57:28
 
 import os
 import argparse
@@ -11,23 +11,31 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 import utility
+import normalize
 from models import cnn
 
-def load_training_data(filepath, image_flat_size,
-                        total_classes, samples_per_class):
+def load_training_data(filepath):
 
-    img = np.fromfile(filepath, dtype=np.uint8)
+    data = np.fromfile(filepath, dtype=np.uint8)
+    num_classes = int.from_bytes(data[:4], byteorder='little')
+    num_samples = int.from_bytes(data[4:8], byteorder='little')
+    width       = int.from_bytes(data[8:12], byteorder='little')
+    height      = int.from_bytes(data[12:16], byteorder='little')
+
+    image_flat_size = width * height
+
+    img = data[16:]
     img.shape = (-1, image_flat_size)
 
     img_class = np.zeros(img.shape[0], dtype=np.int8)
-    img_class.shape = (-1, samples_per_class)
-    for i in xrange(total_classes):
+    img_class.shape = (-1, num_samples)
+    for i in range(num_classes):
         img_class[i, ] = i
     img_class.shape = (-1)
 
     # shuffle dataset
     perm = np.random.permutation(img.shape[0])
-    return img[perm], img_class[perm]
+    return num_classes, width, height, img[perm], img_class[perm]
 
 
 if __name__ == '__main__':
@@ -49,19 +57,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # load data and show data
-    img, img_class = load_training_data('dataset/normalized.bin', 751*841, 62, 55)
+    num_classes, width, height, img, img_class = load_training_data('dataset/normalized.bin')
     if args.show_data == True:
         # disply dataset
         fig = plt.figure()
-        for i in xrange(1, 21):
+        for i in range(1, 21):
             a = fig.add_subplot(4, 5, i)
             a.set_title('Label \'%s\'' % utility.SAMPLE(img_class[i]).name)
             plt.axis('off')
-            plt.imshow(img[i].reshape(841, 751), cmap='gray')
+            plt.imshow(img[i].reshape(height, width), cmap='gray')
         plt.show()
 
     # create network and prediction parameters
-    x, y, y_true, optimizer = cnn.create_network(841, 751, 62)
+    x, y, y_true, optimizer = cnn.create_network(height, width, num_classes)
 
     y_pred_cls = tf.argmax(y, dimension=1)
     y_true_cls = tf.placeholder(tf.int64, shape=[None])
@@ -96,18 +104,20 @@ if __name__ == '__main__':
         img_class_val = img_class[args.train_size:]
         feed_val = {x : img_val, y_true_cls : img_class_val}
 
+        print('train data size: %d' % img_count)
+        print('validation data size: %d' % img_val.shape[0])
+
         # convert to onehot encoding
-        onehot = np.zeros((img_count, 62))
+        onehot = np.zeros((img_count, num_classes))
         onehot[np.arange(img_count), img_class_train] = 1
 
         best_accuracy = 0.0
-        for i in xrange(args.iterations):
+        for i in range(args.iterations):
             perm = np.random.permutation(args.batch_size)
             input_x = img_train[perm]
             input_y = onehot[perm]
             feed = {x : input_x, y_true: input_y}
             session.run(optimizer, feed_dict=feed)
-
             # on every 100 iteration validate training
             if i % 100 == 0:
                 acc = session.run(accuracy, feed_dict=feed_val)
@@ -116,5 +126,5 @@ if __name__ == '__main__':
                     msg = '*'
                     best_accuracy = acc
                     saver.save(sess=session, save_path=save_path)
-                print "validation accuracy #{0}: {2}{1:.9%}".format(i + 1, acc, msg)
+                print("validation accuracy #{0}: {2}{1:.9%}".format(i + 1, acc, msg))
 