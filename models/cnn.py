f1e1e9d0a21cfdc3e1592d1082c00a3510aeceb2
Aman Priyadarshi <aman.eureka@gmail.com>
2017-05-19 00:30:08 +0530

    added test model code tabs and spaces inconsistency

--- models/cnn.py
+++ models/cnn.py
@@ -2,109 +2,109 @@
 # @Author: Aman Priyadarshi
 # @Date:   2017-04-17 18:18:50
 # @Last Modified by:   amaneureka
-# @Last Modified time: 2017-04-26 04:56:16
+# @Last Modified time: 2017-05-19 00:19:44
 
 import numpy as np
 import tensorflow as tf
 
 def cnn_layer(input, num_channels, num_filters, filter_shape):
 
-    # shape = [filter_height, filter_width, input_channels, output_channels]
-    kernel_shape = [filter_shape[0], filter_shape[1], num_channels, num_filters]
-    weights = tf.Variable(tf.random_normal(kernel_shape))
-    biases = tf.Variable(tf.random_normal(shape=[num_filters]))
+		# shape = [filter_height, filter_width, input_channels, output_channels]
+		kernel_shape = [filter_shape[0], filter_shape[1], num_channels, num_filters]
+		weights = tf.Variable(tf.random_normal(kernel_shape))
+		biases = tf.Variable(tf.random_normal(shape=[num_filters]))
 
-    layer = tf.nn.conv2d(input=input,
-                         filter=weights,
-                         strides=[1, 1, 1, 1],
-                         padding='SAME')
-    layer = tf.add(layer, biases)
-    return layer, weights
+		layer = tf.nn.conv2d(input=input,
+												filter=weights,
+												strides=[1, 1, 1, 1],
+												padding='SAME')
+		layer = tf.add(layer, biases)
+		return layer, weights
 
 
 def fc_layer(input, num_input, num_output):
 
-    weights = tf.Variable(tf.random_normal(shape=[num_input, num_output]))
-    biases = tf.Variable(tf.random_normal(shape=[num_output]))
-    layer = tf.add(tf.matmul(input, weights), biases)
-    return layer, weights
+		weights = tf.Variable(tf.random_normal(shape=[num_input, num_output]))
+		biases = tf.Variable(tf.random_normal(shape=[num_output]))
+		layer = tf.add(tf.matmul(input, weights), biases)
+		return layer, weights
 
 def inception2d(input, num_channels, num_filter):
-    # bias dimension = 3*num_filter and then the extra num_channels for the avg pooling
-    bias = tf.Variable(tf.random_normal([3*num_filter + num_channels]))
+		# bias dimension = 3*num_filter and then the extra num_channels for the avg pooling
+		bias = tf.Variable(tf.random_normal([3*num_filter + num_channels]))
 
-    # 1x1
-    one_filter = tf.Variable(tf.random_normal([1, 1, num_channels, num_filter]))
-    one_by_one = tf.nn.conv2d(input, one_filter, strides=[1, 1, 1, 1], padding='SAME')
+		# 1x1
+		one_filter = tf.Variable(tf.random_normal([1, 1, num_channels, num_filter]))
+		one_by_one = tf.nn.conv2d(input, one_filter, strides=[1, 1, 1, 1], padding='SAME')
 
-    # 3x3
-    three_filter = tf.Variable(tf.random_normal([3, 3, num_channels, num_filter]))
-    three_by_three = tf.nn.conv2d(input, three_filter, strides=[1, 1, 1, 1], padding='SAME')
+		# 3x3
+		three_filter = tf.Variable(tf.random_normal([3, 3, num_channels, num_filter]))
+		three_by_three = tf.nn.conv2d(input, three_filter, strides=[1, 1, 1, 1], padding='SAME')
 
-    # 5x5
-    five_filter = tf.Variable(tf.random_normal([5, 5, num_channels, num_filter]))
-    five_by_five = tf.nn.conv2d(input, five_filter, strides=[1, 1, 1, 1], padding='SAME')
+		# 5x5
+		five_filter = tf.Variable(tf.random_normal([5, 5, num_channels, num_filter]))
+		five_by_five = tf.nn.conv2d(input, five_filter, strides=[1, 1, 1, 1], padding='SAME')
 
-    # avg pooling
-    pooling = tf.nn.avg_pool(input, ksize=[1, 3, 3, 1], strides=[1, 1, 1, 1], padding='SAME')
+		# avg pooling
+		pooling = tf.nn.avg_pool(input, ksize=[1, 3, 3, 1], strides=[1, 1, 1, 1], padding='SAME')
 
-    input = tf.concat([one_by_one, three_by_three, five_by_five, pooling], axis=3)  # Concat in the 4th dim to stack
-    input = tf.nn.bias_add(input, bias)
-    return tf.nn.relu(input)
+		input = tf.concat([one_by_one, three_by_three, five_by_five, pooling], axis=3)  # Concat in the 4th dim to stack
+		input = tf.nn.bias_add(input, bias)
+		return tf.nn.relu(input)
 
 
 def create_network(img_height, img_width, num_classes):
 
-    x = tf.placeholder(tf.float32, shape=[None, img_height * img_width])
-    y_true = tf.placeholder(tf.float32, shape=[None, num_classes])
+		x = tf.placeholder(tf.float32, shape=[None, img_height * img_width])
+		y_true = tf.placeholder(tf.float32, shape=[None, num_classes])
 
-    # input tensor
-    tensor = tf.reshape(x, shape=[-1, img_height, img_width, 1])
+		# input tensor
+		tensor = tf.reshape(x, shape=[-1, img_height, img_width, 1])
 
-    # first CNN layer
-    layer, w1 = cnn_layer(input=tensor,
-                           num_channels=1,
-                           num_filters=16,
-                           filter_shape=(3, 3))
-    layer = tf.nn.max_pool(value=layer,
-                           ksize=[1, 3, 3, 1],
-                           strides=[1, 1, 1, 1],
-                           padding='SAME')
-    layer = tf.nn.relu(layer)
+		# first CNN layer
+		layer, w1 = cnn_layer(input=tensor,
+													num_channels=1,
+													num_filters=16,
+													filter_shape=(3, 3))
+		layer = tf.nn.max_pool(value=layer,
+													ksize=[1, 3, 3, 1],
+													strides=[1, 1, 1, 1],
+													padding='SAME')
+		layer = tf.nn.relu(layer)
 
-    # second CNN layer
-    layer, w2 = cnn_layer(input=layer,
-                           num_channels=16,
-                           num_filters=10,
-                           filter_shape=(3, 3))
-    layer = tf.nn.max_pool(value=layer,
-                           ksize=[1, 2, 2, 1],
-                           strides=[1, 1, 1, 1],
-                           padding='SAME')
-    # layer = tf.nn.dropout(layer, 0.75)
-    layer = tf.nn.relu(layer)
+		# second CNN layer
+		layer, w2 = cnn_layer(input=layer,
+													num_channels=16,
+													num_filters=10,
+													filter_shape=(3, 3))
+		layer = tf.nn.max_pool(value=layer,
+													ksize=[1, 2, 2, 1],
+													strides=[1, 1, 1, 1],
+													padding='SAME')
+		# layer = tf.nn.dropout(layer, 0.75)
+		layer = tf.nn.relu(layer)
 
-    # Inception Module
-    layer = inception2d(input=layer,
-                         num_channels=10, 
-                         num_filter=16)
+		# Inception Module
+		layer = inception2d(input=layer,
+												num_channels=10,
+												num_filter=16)
 
-    # shape = [images, height, width, channels]
-    features = layer.get_shape()[1:].num_elements()
-    layer = tf.reshape(layer, shape=[-1, features])
+		# shape = [images, height, width, channels]
+		features = layer.get_shape()[1:].num_elements()
+		layer = tf.reshape(layer, shape=[-1, features])
 
-    # two fully connected layers
-    layer, w4 = fc_layer(input=layer,
-                          num_input=features,
-                          num_output=500)
-    layer = tf.nn.tanh(layer)
-    layer, w6 = fc_layer(input=layer,
-                          num_input=500,
-                          num_output=num_classes)
-    y = tf.nn.softmax(layer)
+		# two fully connected layers
+		layer, w4 = fc_layer(input=layer,
+													num_input=features,
+													num_output=500)
+		layer = tf.nn.tanh(layer)
+		layer, w6 = fc_layer(input=layer,
+													num_input=500,
+													num_output=num_classes)
+		y = tf.nn.softmax(layer)
 
-    # learning
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer, labels=y_true)
-    cost = tf.reduce_mean(cross_entropy)
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=0.0025).minimize(cost)
-    return x, y, y_true, optimizer
+		# learning
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=layer, labels=y_true)
+		cost = tf.reduce_mean(cross_entropy)
+		optimizer = tf.train.RMSPropOptimizer(learning_rate=0.0025).minimize(cost)
+		return x, y, y_true, optimizer
--- train.py
+++ train.py
@@ -2,8 +2,8 @@
 # @Author: Aman Priyadarshi
 # @Date:   2017-04-17 11:39:30
 # @Last Modified by:   amaneureka
-# @Last Modified time: 2017-04-24 13:31:32
-
+# @Last Modified time: 2017-05-19 00:20:39
+import time
 import os
 import sys
 import argparse
@@ -16,121 +16,125 @@ from models import cnn
 
 def load_training_data(filepath):
 
-    data = np.fromfile(filepath, dtype=np.uint8)
-    num_classes = int.from_bytes(data[:4], byteorder='little')
-    num_samples = int.from_bytes(data[4:8], byteorder='little')
-    width       = int.from_bytes(data[8:12], byteorder='little')
-    height      = int.from_bytes(data[12:16], byteorder='little')
+	data = np.fromfile(filepath, dtype=np.uint8)
+	num_classes	= int.from_bytes(data[:4], byteorder='little')
+	num_samples	= int.from_bytes(data[4:8], byteorder='little')
+	width		= int.from_bytes(data[8:12], byteorder='little')
+	height		= int.from_bytes(data[12:16], byteorder='little')
 
-    image_flat_size = width * height
+	image_flat_size = width * height
 
-    img = data[16:]
-    img.shape = (-1, image_flat_size)
+	img = data[16:]
+	img.shape = (-1, image_flat_size)
 
-    img_class = np.zeros(img.shape[0], dtype=np.int8)
-    img_class.shape = (-1, num_samples)
-    for i in range(num_classes):
-        img_class[i, ] = i
-    img_class.shape = (-1)
+	img_class = np.zeros(img.shape[0], dtype=np.int8)
+	img_class.shape = (-1, num_samples)
+	for i in range(num_classes):
+		img_class[i, ] = i
+	img_class.shape = (-1)
 
-    # shuffle dataset
-    perm = np.random.permutation(img.shape[0])
-    return num_classes, width, height, img[perm], img_class[perm]
+	# shuffle dataset
+	perm = np.random.permutation(img.shape[0])
+	return num_classes, width, height, img[perm], img_class[perm]
 
 
 if __name__ == '__main__':
 
-    # parse command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file', action='store', dest='saver_file',
-                        default='validations', help='saver file name')
-    parser.add_argument('--restore', action='store', dest='restore',
-                        help='restore last session')
-    parser.add_argument('--savedir', action='store', dest='savedir',
-                        help='save directory', default='output')
-    parser.add_argument('--dataset', action='store', dest='dataset',
-                        help='dataset load directory', default='dataset')
-    parser.add_argument('--train', action='store_true', dest='train',
-                        default=False, help='train model')
-    parser.add_argument('--show', action='store_true', dest='show_data',
-                        help='display loaded dataset', default=False)
-    parser.add_argument('--iterations', action='store', dest='iterations',
-                        help='training epochs count', type=int, default=50000)
-    args = parser.parse_args()
+	# parse command line arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--file', action='store', dest='saver_file',
+						default='validations', help='saver file name')
+	parser.add_argument('--restore', action='store', dest='restore',
+						help='restore last session')
+	parser.add_argument('--savedir', action='store', dest='savedir',
+						help='save directory', default='output')
+	parser.add_argument('--dataset', action='store', dest='dataset',
+						help='dataset load directory', default='dataset')
+	parser.add_argument('--train', action='store_true', dest='train',
+						default=False, help='train model')
+	parser.add_argument('--show', action='store_true', dest='show_data',
+						help='display loaded dataset', default=False)
+	parser.add_argument('--iterations', action='store', dest='iterations',
+						help='training epochs count', type=int, default=50000)
+	args = parser.parse_args()
 
-    # load data and show data
-    train_dataset = os.path.join(args.dataset, 'normalized-train.bin')
-    validation_dataset = os.path.join(args.dataset, 'normalized-val.bin')
+	# load data and show data
+	train_dataset = os.path.join(args.dataset, 'normalized-train.bin')
+	validation_dataset = os.path.join(args.dataset, 'normalized-val.bin')
 
-    num_classes, width, height, img, img_class = load_training_data(train_dataset)
-    if args.show_data == True:
-        # disply dataset
-        fig = plt.figure()
-        for i in range(1, 21):
-            a = fig.add_subplot(4, 5, i)
-            a.set_title('Label \'%s\'' % utility.SAMPLE(img_class[i]).name)
-            plt.axis('off')
-            plt.imshow(img[i].reshape(height, width), cmap='gray')
-        plt.show()
+	num_classes, width, height, img, img_class = load_training_data(train_dataset)
+	if args.show_data == True:
+		# disply dataset
+		fig = plt.figure()
+		for i in range(1, 21):
+			a = fig.add_subplot(4, 5, i)
+			a.set_title('Label \'%s\'' % utility.SAMPLE(img_class[i]).name)
+			plt.axis('off')
+			plt.imshow(img[i].reshape(height, width), cmap='gray')
+		plt.show()
 
-    # create network and prediction parameters
-    x, y, y_true, optimizer = cnn.create_network(height, width, num_classes)
+	# create network and prediction parameters
+	x, y, y_true, optimizer = cnn.create_network(height, width, num_classes)
 
-    y_pred_cls = tf.argmax(y, dimension=1)
-    y_true_cls = tf.placeholder(tf.int64, shape=[None])
-    correct_predictions = tf.equal(y_true_cls, y_pred_cls)
-    accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+	y_pred_cls = tf.argmax(y, dimension=1)
+	y_true_cls = tf.placeholder(tf.int64, shape=[None])
+	correct_predictions = tf.equal(y_true_cls, y_pred_cls)
+	accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
-    # tensorflow session saver
-    saver = tf.train.Saver()
+	# tensorflow session saver
+	saver = tf.train.Saver()
 
-    if not os.path.exists(args.savedir):
-        os.makedirs(args.savedir)
-    # tmp line
-    from shutil import copyfile
-    copyfile('models/cnn.py', '/output/cnn.py')
-    save_path = os.path.join(args.savedir, args.saver_file)
+	if not os.path.exists(args.savedir):
+		os.makedirs(args.savedir)
 
-    # create tensorflow session
-    session = tf.Session()
-    session.run(tf.global_variables_initializer())
+	save_path = os.path.join(args.savedir, args.saver_file)
 
-    # do we need to restore session?
-    if args.restore is not None:
-        saver.restore(sess=session, save_path=args.restore)
+	# create tensorflow session
+	session = tf.Session()
+	session.run(tf.global_variables_initializer())
 
-    # train network of requested size
-    if args.train > 0:
+	# do we need to restore session?
+	if args.restore is not None:
+		saver.restore(sess=session, save_path=args.restore)
 
-        # load datasets
-        _, _, _, img_val, img_class_val = load_training_data(validation_dataset)
+	# train network of requested size
+	if args.train == True:
 
-        img_count = img.shape[0]
-        feed_val = {x : img_val, y_true_cls : img_class_val}
+		# load datasets
+		_, _, _, img_val, img_class_val = load_training_data(validation_dataset)
 
-        print('train data size: %d' % img_count)
-        print('validation data size: %d' % img_val.shape[0])
+		img_count = img.shape[0]
+		feed_val = {x : img_val, y_true_cls : img_class_val}
 
-        # convert to onehot encoding
-        onehot = np.zeros((img_count, num_classes))
-        onehot[np.arange(img_count), img_class] = 1
+		print('train data size: %d' % img_count)
+		print('validation data size: %d' % img_val.shape[0])
 
-        training_acc = 0.0
-        best_accuracy = 0.0
-        for i in range(args.iterations):
-            feed = {x : img, y_true: onehot, y_true_cls: img_class}
-            _, tacc = session.run([optimizer, accuracy], feed_dict=feed)
-            training_acc = training_acc + tacc
-            # on every 100 iteration validate training
-            if i % 100 == 0:
-                acc = session.run(accuracy, feed_dict=feed_val)
-                msg = ' '
-                training_acc /= 100.0
-                if acc > best_accuracy:
-                    msg = '*'
-                    best_accuracy = acc
-                    saver.save(sess=session, save_path=save_path)
-                print("accuracy #{0}: {3:.9%} {2}{1:.9%}".format(i + 1, acc, msg, training_acc))
-                training_acc = 0.0
-                sys.stdout.flush()
+		# convert to onehot encoding
+		onehot = np.zeros((img_count, num_classes))
+		onehot[np.arange(img_count), img_class] = 1
+
+		training_acc = 0.0
+		best_accuracy = 0.0
+		for i in range(args.iterations):
+			feed = {x : img, y_true: onehot, y_true_cls: img_class}
+			_, tacc = session.run([optimizer, accuracy], feed_dict=feed)
+			training_acc = training_acc + tacc
+			# on every 100 iteration validate training
+			if i % 100 == 0:
+				acc = session.run(accuracy, feed_dict=feed_val)
+				msg = ' '
+				training_acc /= 100.0
+				if acc > best_accuracy:
+					msg = '*'
+					best_accuracy = acc
+					saver.save(sess=session, save_path=save_path)
+				print("accuracy #{0}: {3:.9%} {2}{1:.9%}".format(i + 1, acc, msg, training_acc))
+				training_acc = 0.0
+				sys.stdout.flush()
+
+	else:
+		# test on some random data
+		feed_val = {x : img[0].reshape(1, -1)}
+		y_prd, prd_cls = session.run([y, y_pred_cls], feed_dict=feed_val)
+		print(img_class[0], prd_cls, y_prd[0, prd_cls - 1])
 
